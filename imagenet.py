08202b868ae5e6128a896fd95de2580416dd6156
Hiroshi Fukui <fhiro@vision.cs.chubu.ac.jp>
2018-12-15 12:12:48 +0900

    add file

--- imagenet.py
+++ imagenet.py
@@ -8,7 +8,10 @@ import argparse
 import os
 import shutil
 import time
+import numpy as np
 import random
+import pickle
+# import cv2
 
 import torch
 import torch.nn as nn
@@ -20,6 +23,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 import models.imagenet as customized_models
+from os import path
 
 from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
 
@@ -31,12 +35,12 @@ default_model_names = sorted(name for name in models.__dict__
 customized_models_names = sorted(name for name in customized_models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(customized_models.__dict__[name]))
-
 for name in customized_models.__dict__:
     if name.islower() and not name.startswith("__") and callable(customized_models.__dict__[name]):
         models.__dict__[name] = customized_models.__dict__[name]
 
-model_names = default_model_names + customized_models_names
+# model_names = default_model_names + customized_models_names
+model_names = customized_models_names
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
@@ -153,11 +157,11 @@ def main():
         print("=> creating model '{}'".format(args.arch))
         model = models.__dict__[args.arch]()
 
-    if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
-        model.features = torch.nn.DataParallel(model.features)
-        model.cuda()
-    else:
-        model = torch.nn.DataParallel(model).cuda()
+    #if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
+    #    model.features = torch.nn.DataParallel(model.features)
+    #    model.cuda()
+    # else:
+    model = torch.nn.DataParallel(model).cuda()
 
     cudnn.benchmark = True
     print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
@@ -241,8 +245,12 @@ def train(train_loader, model, criterion, optimizer, epoch, use_cuda):
         inputs, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(targets)
 
         # compute output
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
+        att_outputs, outputs, _  = model(inputs)
+        att_loss = criterion(att_outputs, targets)
+        per_loss = criterion(outputs, targets)
+        loss = att_loss + per_loss
+
+
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
@@ -277,6 +285,7 @@ def train(train_loader, model, criterion, optimizer, epoch, use_cuda):
 
 def test(val_loader, model, criterion, epoch, use_cuda):
     global best_acc
+    softmax = nn.Softmax()
 
     batch_time = AverageMeter()
     data_time = AverageMeter()
@@ -284,11 +293,22 @@ def test(val_loader, model, criterion, epoch, use_cuda):
     top1 = AverageMeter()
     top5 = AverageMeter()
 
+    fc = open('categorie.txt', 'r')
+    cate_name = []
+    for line in fc:
+        idx_item, name_item = line[:-2].split(':')
+        sp_name = name_item.split(',')
+        name_item = sp_name[0]
+        cate_name.append(name_item)
+    cate_name = np.asarray(cate_name)
+
     # switch to evaluate mode
     model.eval()
 
     end = time.time()
     bar = Bar('Processing', max=len(val_loader))
+    count = 0
+    info_count = 0
     for batch_idx, (inputs, targets) in enumerate(val_loader):
         # measure data loading time
         data_time.update(time.time() - end)
@@ -298,11 +318,46 @@ def test(val_loader, model, criterion, epoch, use_cuda):
         inputs, targets = torch.autograd.Variable(inputs, volatile=True), torch.autograd.Variable(targets)
 
         # compute output
-        outputs = model(inputs)
+        _, outputs, attention = model(inputs)
+        outputs = softmax(outputs)
         loss = criterion(outputs, targets)
+        # attention, fe, per = attention
+
+        c_att = attention.data.cpu()
+        c_att = c_att.numpy()
+        d_inputs = inputs.data.cpu()
+        d_inputs = d_inputs.numpy()
+
+        in_b, in_c, in_y, in_x = inputs.shape
+        for item_img, item_att in zip(d_inputs, c_att):
+
+            v_img = ((item_img.transpose((1,2,0)) + 0.5 + [0.485, 0.456, 0.406]) * [0.229, 0.224, 0.225])* 256
+            v_img = v_img[:, :, ::-1]
+            resize_att = cv2.resize(item_att[0], (in_x, in_y))
+            resize_att *= 255.
+
+            cv2.imwrite('stock1.png', v_img)
+            cv2.imwrite('stock2.png', resize_att)
+            v_img = cv2.imread('stock1.png')
+            vis_map = cv2.imread('stock2.png', 0)
+            jet_map = cv2.applyColorMap(vis_map, cv2.COLORMAP_JET)
+            jet_map = cv2.add(v_img, jet_map)
+
+            out_dir = path.join('output')
+            if not path.exists(out_dir):
+                os.mkdir(out_dir)
+            out_path = path.join(out_dir, 'attention', '{0:06d}.png'.format(count))
+            cv2.imwrite(out_path, jet_map)
+            out_path = path.join(out_dir, 'raw', '{0:06d}.png'.format(count))
+            cv2.imwrite(out_path, v_img)
+
+            count += 1
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+        top5_list = outputs.data.topk(5, 1, True, True)
+        sample_idx = 0
+
         losses.update(loss.data[0], inputs.size(0))
         top1.update(prec1[0], inputs.size(0))
         top5.update(prec5[0], inputs.size(0))
